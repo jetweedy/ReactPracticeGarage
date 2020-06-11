44d74dc7ed6ef56a98d66379d2ca72c8109e9f12
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-06-11 15:05:48 -0400

    Add files via upload

--- src/Car.js
+++ src/Car.js
@@ -7,6 +7,18 @@ class Car extends React.Component {
         this.setState({ color:color });
         this.state.color = color;
     }
+    alertDirectArrow = (x, ev) => {
+        alert(x + " | " + ev.type);
+    }
+    alertYearBound(x) {
+        alert(x);
+    }
+    alertYearScopedInline() {
+        alert(this.state.year);
+    }
+    alertYearScopedInClass = () => {
+        alert(this.state.year);
+    }
     constructor(props) {
       super(props);
       this.state = {
@@ -18,7 +30,17 @@ class Car extends React.Component {
     }
 
     render() {
-        return <li style={{color: this.state.color}}><a onClick={this.paint}>{this.state.brand} {this.state.model}</a> <button onClick={() => alert(this.state.year)}>Alert Year</button></li>;
+        return (<li style={{color: this.state.color}}>
+            <a onClick={this.paint}>{this.state.brand} {this.state.model}</a>
+            <br />
+            <button onClick={(ev) => this.alertDirectArrow(this.state.year, ev)}>Direct arrow function call</button>
+            <br />
+            <button onClick={this.alertYearBound.bind(this,this.state.year)}>Bind argument to generate curried function.</button>
+            <br />
+            <button onClick={() => this.alertYearScopedInline()}>Inirect call to function as method of 'this'.</button>
+            <br />
+            <button onClick={this.alertYearScopedInClass}>Direct reference to pre-scoped function.</button>
+            </li>);
     }
 }
 
